9e9f1397d65948aacc5f9be91f3c397c524d1b64
qianlin02 <hyl2236841196@outlook.com>
2023-04-15 23:28:21 +0800

    .

---
+++ 4.15/简单计算机-4.15/FileName.c
@@ -0,0 +1,49 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//简单的计算机
+//int main()
+//{
+//	int a,b,f=0;
+//	char op;
+//	
+//	scanf("%d", &a);
+//	while (1)
+//	{
+//		scanf("%c", &op);
+//		if (op == '=')
+//		{
+//			break;
+//		}
+//		scanf("%d", &b);
+//		switch(op)
+//		{
+//		case '+':
+//			a += b;
+//			break;
+//		case '-':
+//			a -= b;
+//			break;
+//		case '*':
+//			a *= b;
+//			break;
+//		case '/':
+//			if (b == 0)
+//				f = 1;
+//			else
+//				a /= b;
+//			break;
+//		default:
+//			f = 1;
+//			break;
+//		}
+//		if (f == 1)
+//			break;
+//	}
+//	if (f == 1)
+//		printf("ERROR");
+//	else
+//		printf("%d", a);
+//	return 0;
+//}
