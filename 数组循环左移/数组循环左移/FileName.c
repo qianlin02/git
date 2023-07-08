9f4bfffa59fc518569ea8e392ce1bd690045100e
qianlin02 <hyl2236841196@outlook.com>
2023-07-08 09:38:03 +0800

    7.8

---
+++ 数组循环左移/数组循环左移/FileName.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	int n, m;
+	int i,j;
+	int* a;
+	int temp;
+	scanf("%d %d", &n,&m);
+	a = (int*)calloc(n, sizeof(int));
+	if (a == NULL)
+	{
+		printf("%s", strerror(errno));
+	}
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	for (i = 0; i < m; i++)
+	{
+		temp = a[0];
+		for (j = 1; j < n; j++)
+		{
+			a[j - 1] = a[j];
+		}
+		a[n-1] = temp;
+	}
+	for (i = 0; i < n; i++)
+	{
+
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(" ");
+	}
+	return 0;
+}
